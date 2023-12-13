dd4f504bc8d741190d4f0bd1040cff1135df2a33
pety02 <pety02@abv.bg>
2023-12-13 13:52:16 +0200

    Commit init

--- src/main/java/com/example/bankaccountmanager/BankAccountManagerApplication.java
+++ src/main/java/com/example/bankaccountmanager/BankAccountManagerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankAccountManagerApplication {
 
+    // init
     public static void main(String[] args) {
         SpringApplication.run(BankAccountManagerApplication.class, args);
     }
